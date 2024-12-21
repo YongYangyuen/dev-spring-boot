3f20699d8f315a68eb73fe061ac87e0a5c341366
Yong Yangyuen <kkingza@hotmail.com>
2024-12-22 01:49:08 +0700

    Add @PostConstruct for init method, and add @PreDestroy for destroy method.

--- src/main/java/com/luv2code/springboot/demo/utils/TeacherImpl.java
+++ src/main/java/com/luv2code/springboot/demo/utils/TeacherImpl.java
@@ -1,11 +1,25 @@
 package com.luv2code.springboot.demo.utils;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TeacherImpl implements Teacher {
     public TeacherImpl() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // Define our init method.
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // Define our destroy method.
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @Override
