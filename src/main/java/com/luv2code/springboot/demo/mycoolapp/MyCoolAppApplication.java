81fcbc01190e2db366be25c40f4cbcc215538dd9
Yong Yangyuen <kkingza@hotmail.com>
2025-01-08 00:33:51 +0700

    Connect to database student_tracker table, and turn off Spring Boot banner & reduce logging level to only warning error message by config.

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MyCoolAppApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MyCoolAppApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 // Can use 2 ways for all components scan
 //@SpringBootApplication(scanBasePackages = {"com.luv2code.springboot.demo.mycoolapp", "com.luv2code.springboot.demo.utils"})
@@ -10,6 +12,13 @@ public class MyCoolAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyCoolAppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World.");
+        };
     }
 
 }
