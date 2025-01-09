69ffbd5ea15a8ea66b2ec9061972be71abbb7e9b
Yong Yangyuen <kkingza@hotmail.com>
2025-01-10 02:20:29 +0700

    Add lombok lib, and create student entity for Object-Relational Mapping (ORM).

---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/entities/Student.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.demo.mycoolapp.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
