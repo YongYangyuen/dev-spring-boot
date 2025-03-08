535c945a9bb5bd5ca6e8c30cb4a0b2d35915eb1f
Yong Yangyuen <kkingza@hotmail.com>
2025-03-08 23:38:15 +0700

    Apply @PostConstruct to load the secret student data before initializing bean.

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controllers/StudentRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controllers/StudentRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.demo.mycoolapp.controllers;
 import com.luv2code.springboot.demo.mycoolapp.daos.StudentDAO;
 import com.luv2code.springboot.demo.mycoolapp.entities.Student;
 import com.luv2code.springboot.demo.mycoolapp.models.StudentResponse;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,17 @@ import java.util.List;
 public class StudentRestController {
     private final StudentDAO studentDAO;
 
+    private List<Student> students;
+
     public StudentRestController(StudentDAO studentDAO) {
         this.studentDAO = studentDAO;
+    }
+
+    // Define @PostConstruct to load the secret student data ... only once!
+    @PostConstruct
+    public void loadSecretStudentData() {
+        students = new ArrayList<>();
+        students.add(new Student("XXX", "YYY", "ZZZ"));
     }
 
     // Define endpoint for "/students": return a list of students.
@@ -29,11 +39,16 @@ public class StudentRestController {
     }
 
     @GetMapping("/students/{id}")
-    public StudentResponse getStudentById(@PathVariable Integer id) {
+    public StudentResponse getStudentById(@PathVariable int id) {
         Student student = studentDAO.findById(id);
         List<Student> students = new ArrayList<>();
         students.add(student);
 
+        return new StudentResponse(students);
+    }
+
+    @GetMapping("/students/secret")
+    public StudentResponse getSecretStudent() {
         return new StudentResponse(students);
     }
 }
