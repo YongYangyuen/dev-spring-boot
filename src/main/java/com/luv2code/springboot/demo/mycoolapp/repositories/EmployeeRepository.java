b85aeed23ef8c587af256571307a3991f59c5748
Yong Yangyuen <kkingza@hotmail.com>
2025-04-04 13:39:51 +0700

    Use Spring Data REST to configs and sorting.

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/repositories/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/repositories/EmployeeRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // JPA Repository is used to minimize the boilerplate code in dao files (both interface and implementation files are not needed anymore).
 // JpaRepository<Employee, Integer> => Employee: Entity type, Integer: Primary key.
+// Using @RepositoryRestResource to change default path (Entity name in plural: employees) for Spring Data REST.
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // JPA Repository has CRUD methods (findAll, findById, save, deleteById, and others) by default, so we don't need to implement CRUD methods by ourselves.
 }
